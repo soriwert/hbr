b0357fe91e418280c943136a787ab916b5a180c4
SORIANO <SORIANO@192.168.0.158>
2017-04-12 01:41:43 +0200

    subir

--- Hiberus/ejerCampusN2t2/src/Operaciones.java
+++ Hiberus/ejerCampusN2t2/src/Operaciones.java
@@ -58,10 +58,39 @@ public class Operaciones  extends CreaVector{
 			k--;
 		}
 	}
-	
+
+	public void setRepetidosInt(){
+		int[] prueba={1,2,3,1,2,3,3,7,9};
+		int[] prueba2={200,200,200,200,200,200,200,200,200};
+		int contador=0;
+		boolean salir=false;
+
+
+
+
+		for (int i=0;i<prueba.length;i++){
+			for (int j=i+1; j<prueba.length-1;j++){
+				
+				if(prueba[i]==prueba[j]){
+					for(int k=0;k<prueba2.length-1;k++){
+						if(prueba2[k]!=prueba[i]){
+					prueba2[contador]=prueba[i];
+					contador++;
+					j=prueba.length;
+					k=prueba.length;
+						}
+					}
+				}
+			}
+			System.out.println(prueba2[i]);
+		}
+	}
+
+
+
 	//7. Write a Java program to find the duplicate 
 	//values of an array of string values.
-	public String setRepetidos(){
+	public String setRepetidosString(){
 		return "";
 	}
 }
--- Hiberus/pruebas/src/pruebas/Pruebas.java
+++ Hiberus/pruebas/src/pruebas/Pruebas.java
@@ -4,19 +4,30 @@ public class Pruebas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String saludo="hola que tal";
-		String almacen="";
-		for (int i=0; i<saludo.length();i++){
-			for(int j=i+1; j<saludo.length();j++){
-				if(saludo.charAt(j)==saludo.charAt(i)){
-					
-					almacen+=saludo.charAt(i);
-					
+		int[] prueba={1,2,3,1,2,3,3,7,9};
+		int[] prueba2={200,200,200,200,200,200,200,200,200};
+		int contador=0;
+		boolean salir=false;
+
+
+
+
+		for (int i=0;i<prueba.length;i++){
+			for (int j=i+1; j<prueba.length-1;j++){
+				
+				if(prueba[i]==prueba[j]){
+					for(int k=0;k<prueba2.length-1;k++){
+						if(prueba2[k]!=prueba[i]){
+					prueba2[contador]=prueba[i];
+					contador++;
+					j=prueba.length;
+					k=prueba.length;
+						}
+					}
 				}
 			}
+			System.out.println(prueba2[i]);
 		}
-
-		System.out.println(almacen);
 
 	}
 
